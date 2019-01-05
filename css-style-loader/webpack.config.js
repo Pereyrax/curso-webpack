81cca2e7c9d01f9d987976c660252b4450455c29
Gabriel <pereyrarg11@gmail.com>
2019-01-05 17:57:31 -0600

    loaders

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // listar los loaders
+      {
+        // test: tipo de archivo a reconocer,
+        // use: loader encargado del archivo
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
